88c97f281153854662cba4e916495c519429d9ed
Sanjay Maddheshiya <coder.sanjay19@gmail.com>
2023-07-04 18:51:34 +0530

    changing api key

--- lib/constant/api_constant.dart
+++ lib/constant/api_constant.dart
@@ -1,4 +1,4 @@
  String BASE_URL = "https://api.openai.com/v1";
 //  String API_KEY = "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";
- String API_KEY = "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";
+ String API_KEY = "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";
 
